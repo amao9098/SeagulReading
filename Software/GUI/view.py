ab8bae01fc78115ad1d5bde18c67e5b4b4b0b7eb
amao9098 <amao9098@gmail.com>
2018-06-29 10:21:50 -0700

    Update view.py

--- Software/GUI/view.py
+++ Software/GUI/view.py
@@ -13,14 +13,18 @@ from model import Model
 class SeagullFrame(wx.Frame):
 
     def __init__(self):
-        wx.Frame.__init__(self, None, wx.ID_ANY, "Seagull Reading")
+        width, height = wx.GetDisplaySize()
+        wx.Frame.__init__(self, None, wx.ID_ANY, "Seagull Reading", size =(width, height))
         self.panel = wx.Panel(self)
         self.Bind(wx.EVT_CLOSE, self.on_close)
+        buttonFont = wx.Font(30, wx.MODERN, wx.NORMAL, wx.NORMAL)
         self.rest_btn = wx.Button(self.panel, label="Start 5-minute Resting")
         self.read_btn = wx.Button(self.panel, label="Start Reading")
+        self.rest_btn.SetFont(buttonFont)
+        self.read_btn.SetFont(buttonFont)
         ### GUI ###
         self.title = wx.StaticText(self.panel, label="Seagull Reading")
-        font = wx.Font(18, wx.MODERN, wx.NORMAL, wx.BOLD)
+        font = wx.Font(40, wx.MODERN, wx.NORMAL, wx.BOLD)
         self.title.SetFont(font)
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.title, 1, wx.ALL | wx.ALIGN_CENTER, 5)
@@ -55,7 +59,7 @@ class SeagullFrame(wx.Frame):
             read_window = ReadingWindow(self.model, 1)
             read_window.Show()
             # start detection
-            self.model.start_reading()
+            # self.model.start_reading()
 
     def on_close(self, event):
         self.Destroy()
@@ -64,21 +68,23 @@ class SeagullFrame(wx.Frame):
 class ReadingWindow(wx.Frame):
 
     def __init__(self, model, text_num):
-        wx.Frame.__init__(self, None, wx.ID_ANY, "Please enter your information:", size=(650, 300))
+        self.width, self.height = wx.GetDisplaySize()
+        wx.Frame.__init__(self, None, wx.ID_ANY, "Passage Reading", size=(self.width, self.height))
         self.model = model
         self.panel = wx.Panel(self)
         self.Bind(wx.EVT_CLOSE, self.on_close)
         # text panel
         self._shown_text_idx = 0
         self._shown_text = wx.StaticText(self.panel, label=self.model.get_text(self._shown_text_idx))
-        self._shown_text.Wrap(500)
-        font = wx.Font(14, wx.MODERN, wx.NORMAL, wx.BOLD)
+        self._shown_text.Wrap(self.width - 50)
+        font = wx.Font(28, wx.MODERN, wx.NORMAL, wx.BOLD)
         self._shown_text.SetFont(font)
         # next button
         self._next_button = wx.Button(self.panel, label="Next line")
+        self._next_button.SetFont(font)
         self.sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.sizer.Add(self._shown_text, 10, wx.EXPAND | wx.ALL, border=10)
-        self.sizer.Add(self._next_button, 0, wx.RIGHT | wx.ALL, border=10)
+        self.sizer.Add(self._next_button, 0, wx.ALIGN_BOTTOM | wx.ALL, border=40)
         self.panel.SetSizer(self.sizer)
         # button listener
         self._next_button.Bind(wx.EVT_BUTTON, self.on_next_line)
@@ -89,10 +95,28 @@ class ReadingWindow(wx.Frame):
         self._shown_text_idx += 1
         try:
             self._shown_text.SetLabel(self.model.get_text(self._shown_text_idx))
-            self._shown_text.Wrap(500)
+            self._shown_text.Wrap(self.width - 50)
         except ValueError as e:
-            print(e)
-            wx.MessageBox("This is the last line! You are done", "Warning", wx.OK | wx.ICON_INFORMATION)
+            if self.model.is_intro():
+                self._shown_text.SetLabel("Press Next Line button to start passages")
+                self.model.next_passage()
+                self._shown_text_idx = -1
+                self.model.start_reading()
+            else:
+                self._shown_text.SetLabel("")
+                wx.MessageBox("This is the last line! FINISH GOOGLE SURVEY before clicking NEXT LINE!", "Warning", wx.OK | wx.ICON_INFORMATION)
+                # start another passage
+                self._shown_text_idx = -1
+                if self.model.check_passage_num():
+                    self.model.next_passage()
+                else:
+                    self._shown_text.SetLabel("This was the last passage! Thank you for participating in this experiment!")
+                    self._shown_text.Wrap(self.width - 50)
+                    self.model.finish_reading()
+                    self.sizer.Hide(self._next_button)
+                    self.sizer.Remove(self._next_button)
+                    self.sizer.Layout()
+
 
     def on_close(self, event):
         self.Destroy()
@@ -107,26 +131,38 @@ class ReadingWindow(wx.Frame):
 class StartWindow(wx.Frame):
 
     def __init__(self, model):
-        wx.Frame.__init__(self, None, wx.ID_ANY, "Please enter your information:")
+        self.width = 700
+        self.height = 800
+        wx.Frame.__init__(self, None, wx.ID_ANY, "Please enter your information:", size=(self.width, self.height))
         self.model = model
         self.panel = wx.Panel(self)
         self.Bind(wx.EVT_CLOSE, self.on_close)
+        font = wx.Font(20, wx.MODERN, wx.NORMAL, wx.NORMAL)
         # Subject recording
-        self.name_txt = wx.TextCtrl(self.panel, -1, style=wx.TE_CENTRE, size=(250, -1))
-        self.name_txt.SetValue("Your name (Last, First): ")
+        self.name_txt = wx.TextCtrl(self.panel, -1, style=wx.TE_CENTRE, size=(625, 250))
+        self.name_txt.SetValue("Subject ID (e.g. S1): ")
+        self.name_txt.SetFont(font)
         self.name_txt.Bind(wx.EVT_KEY_DOWN, self.toggle_name)
-        self.exp_txt = wx.TextCtrl(self.panel, -1, style=wx.TE_CENTRE, size=(250, -1))
+        self.exp_txt = wx.TextCtrl(self.panel, -1, style=wx.TE_CENTRE, size=(625, 250))
         self.exp_txt.SetValue("Your experiment number: ")
+        self.exp_txt.SetFont(font)
         self.exp_txt.Bind(wx.EVT_KEY_DOWN, self.toggle_exp)
+        self.version_choices = ['Version A', 'Version B']
+        self.version_box = wx.RadioBox(self.panel, label='Choose Version', choices=self.version_choices, style=wx.RA_SPECIFY_ROWS)
+        self.version_box.SetFont(font)
+        self.version_box.Bind(wx.EVT_RADIOBOX, self.toggle_version)
         # boxer
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.name_txt, 1, wx.ALL|wx.ALIGN_CENTER, 5)
         self.sizer.Add(self.exp_txt, 1, wx.ALL|wx.ALIGN_CENTER, 5)
+        self.sizer.Add(self.version_box, 1, wx.ALL | wx.ALIGN_CENTER, 5)
         self.panel.SetSizer(self.sizer)
         # button
         hbox = wx.BoxSizer(wx.HORIZONTAL)
         self.submit_btn = wx.Button(self.panel, label="Submit")
         self.cancel_btn = wx.Button(self.panel, label="Cancel")
+        self.submit_btn.SetFont(font)
+        self.cancel_btn.SetFont(font)
         hbox.Add(self.submit_btn)
         hbox.Add(self.cancel_btn)
         self.sizer.Add(hbox, 1, wx.ALL|wx.ALIGN_CENTER, 5)
@@ -136,8 +172,14 @@ class StartWindow(wx.Frame):
         # timer used for count down
         self._start_rest_time = None
 
+    def toggle_version(self, event):
+        if self.version_box.GetStringSelection() == 'Version A':
+            self.model.set_version('A')
+        else:
+            self.model.set_version('B')
+
     def toggle_name(self, event):
-        if self.name_txt.GetValue() == "Your name (Last, First): ":
+        if self.name_txt.GetValue() == "Subject ID (e.g. S1): ":
             self.name_txt.SetValue("")
         event.Skip()
 
@@ -158,10 +200,12 @@ class StartWindow(wx.Frame):
             child.Destroy()
         # start timer
         wait_txt = wx.StaticText(self.panel, label="Please wait for resting data collection to be done...")
-        font = wx.Font(10, wx.MODERN, wx.NORMAL, wx.BOLD)
+        font = wx.Font(20, wx.MODERN, wx.NORMAL, wx.BOLD)
         wait_txt.SetFont(font)
+        wait_txt.Wrap(self.width - 50)
         # timing for 5 min
-        while time.time() - self.model.get_start_rest_time() <= 10:
+        # change time to change length of rest recording
+        while time.time() - self.model.get_start_rest_time() <= 60 * 5:
             pass
         # finish resting
         self.model.finish_resting()
@@ -179,4 +223,3 @@ class StartWindow(wx.Frame):
 
 def play_beep(frequency=2500, duration=1000):
     winsound.Beep(frequency, duration)
-
